3b1fd3fc24f9b7008f0ec77353484ca8fd980f38
Tim <murano1980@gmail.com>
2015-02-23 21:02:31 +0300

    tutor from toptal

--- src/main/scala/tutorial/Boot.scala
+++ src/main/scala/tutorial/Boot.scala
@@ -1,10 +1,24 @@
 package tutorial
 
-/**
- * Created by tim on 23.02.15.
- */
-object Boot{
-  def main (args: Array[String]) {
-    println("Start")
+import akka.actor.{ActorSystem, Props}
+
+object Boot extends App {
+
+  import akka.util.Timeout
+  import scala.concurrent.duration._
+  import akka.pattern.ask
+  import akka.dispatch.ExecutionContexts._
+
+  implicit val ec = global
+
+  override def main(args: Array[String]) {
+    val system = ActorSystem("System")
+    val actor = system.actorOf(Props(new WordCounterActor(args(0))))
+    implicit val timeout = Timeout(25 seconds)
+    val future = actor ? StartProcessFileMsg()
+    future.map { result =>
+      println("Total number of words " + result)
+      system.shutdown
+    }
   }
 }
